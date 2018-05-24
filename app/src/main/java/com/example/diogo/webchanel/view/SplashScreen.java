b0de533412760fc649bfb580d66813f307c5d36a
jpaulinho <j.paulinho_ribeiro@hotmail.com>
2018-05-24 03:11:15 -0300

    Ajustes na Tela de Login

--- app/src/main/java/com/example/diogo/webchanel/view/SplashScreen.java
+++ app/src/main/java/com/example/diogo/webchanel/view/SplashScreen.java
@@ -1,4 +1,41 @@
 package com.example.diogo.webchanel.view;
 
-public class SplashScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.diogo.webchanel.R;
+
+import java.util.jar.JarEntry;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        Thread timerThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 }
