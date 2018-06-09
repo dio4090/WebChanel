3df425d17353ac74ee98ba39ad148b93bbd28914
Diogo Rocha de Araujo <diogo.nox42@outlook.com>
2018-06-09 18:15:26 -0300

    Adding Login Service and adjusts in login

---
+++ app/src/main/java/com/example/diogo/webchanel/util/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.example.diogo.webchanel.util;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "https://dellectus.com/android/login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String email, String password, Response.Listener<String> listener) {
+        super(Method.POST, LOGIN_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
--- app/src/main/java/com/example/diogo/webchanel/view/LoginActivity.java
+++ app/src/main/java/com/example/diogo/webchanel/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.diogo.webchanel.view;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.example.diogo.webchanel.R;
+import com.android.volley.Response;
+import com.example.diogo.webchanel.model.User;
+import com.example.diogo.webchanel.util.LoginRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +28,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnLogin;
     TextView txtNewUser;
     TextView txtNewEnterprise;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +50,83 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         txtNewEnterprise.setOnClickListener(this);
     }
 
-    private void validateLogin(String login, String pass) {
+    private void login() {
+        user = new User();
+        String email = "", password = "";
 
+        //Pegando os valores do login
+        email = edtLogin.getText().toString();
+        password = edtPassword.getText().toString();
+
+        //Frame de login
+        loginProess();
+
+        // TODO: Implement your own authentication logic here.
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String strResponse) {
+                try {
+                    JSONArray jsonResponse = new JSONArray(strResponse);
+
+                    //Verifica se o login e valido
+                    //Verifica se o Json esta vazio
+                    if(jsonResponse.length() > 0) {
+                        for (int i = 0; i < jsonResponse.length(); i++) {
+                            JSONObject jsonobject = jsonResponse.getJSONObject(i);
+                            user.setId(jsonobject.getInt("id"));
+                            user.setEmail(jsonobject.getString("email"));
+                            user.setPassword(jsonobject.getString("password"));
+                        }
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    } else {
+                        startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                        userNotExists();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(loginRequest);
+    }
+
+    private void loginProess() {
+        //Dialog
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        //Wait 3 seconds
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onLoginSuccess();
+                        progressDialog.dismiss();
+                    }
+                }, 2000);
+    }
+
+    private void userNotExists(){
+        Toast.makeText(getBaseContext(), "Login ou senha inválidos", Toast.LENGTH_LONG).show();
+        btnLogin.setEnabled(true);
+    }
+
+    public void onLoginSuccess() {
+        btnLogin.setEnabled(true);
+        finish();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                login();
                 break;
 
             case R.id.txt_new_user:
