ece6f955b7b605c834e63cf5b6fe746d083a3b9b
jpaulinho <j.paulinho_ribeiro@hotmail.com>
2018-06-11 17:37:20 -0300

    Ajustes Fragments Schedules, Services and Class Fragment Services

--- app/src/main/java/com/example/diogo/webchanel/view/fragments/FragmentServices.java
+++ app/src/main/java/com/example/diogo/webchanel/view/fragments/FragmentServices.java
@@ -7,14 +7,58 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.diogo.webchanel.R;
 
+import java.util.ArrayList;
+
 public class FragmentServices extends Fragment {
+
+    public FragmentServices(){
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_services, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_services, container, false);
+
+        //Rerefênciamento ao listView e criando o arrayAdapter
+        ListView lista = (ListView) view.findViewById(R.id.lvServices);
+
+        ArrayList<String> enterprises = adicionarEnterprises();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                enterprises);
+
+        lista.setAdapter(arrayAdapter);
+
+        return view;
+
+    }
+
+    //Método para popular o listView
+    private ArrayList<String> adicionarEnterprises() {
+
+        ArrayList<String> dados =  new ArrayList<String>();
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+        dados.add("SERVIÇO TESTE");
+
+        return dados;
+
     }
 }
